df0d4ed870a67686a510e003c62cb9bcd298691b
Esahtengang Asonganyi <93486453+Esahte@users.noreply.github.com>
2024-03-03 19:18:10 -0400

    edited the add.blade.php page

--- resources/views/clients/add.blade.php
+++ resources/views/clients/add.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="px-4 py-5 my-5 bg-body-tertiary rounded-3">
-{{--        <img class="img-fluid" src="{{ asset('images/UWI-Wordmark.webp') }}" alt="UWI Wordmark" width="300"/>--}}
+        {{--        <img class="img-fluid" src="{{ asset('images/UWI-Wordmark.webp') }}" alt="UWI Wordmark" width="300"/>--}}
         <h1 class="display-5 fw-bold text-body-emphasis">Create Client</h1>
         @if($errors->any())
             <div class="alert alert-danger">
@@ -13,35 +13,33 @@
                 </ul>
             </div>
         @endif
-        <div class="col-lg-6 mx-auto">
-            <form action="{{ url('/clients') }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                <div class="mb-3">
-                    <label for="name" class="form-label">Name</label>
-                    <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required>
-                </div>
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email</label>
-                    <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required>
-                </div>
-                <div class="mb-3">
-                    <label for="phone" class="form-label">Phone</label>
-                    <input type="tel" class="form-control" id="phone" name="phone" value="{{ old('phone') }}" required
-                           pattern="^\d{3}-\d{3}-\d{4}$">
-                    <small>Format: 123-456-7890</small>
-                </div>
-                <div class="mb-3">
-                    <label for="address" class="form-label">Address</label>
-                    <textarea class="form-control" id="address" name="address" required
-                              placeholder="3383 Antigua Ave, Five Islands Village, Antigua & Barbuda">{{ old('address') }}</textarea>
-                </div>
-                <div class="mb-3">
-                    <label for="logo" class="form-label">Company Logo</label>
-                    <input type="file" class="form-control" id="logo" name="logo" required>
-                    <small>Only image files (e.g. jpg, png) are allowed, and files must be less than 2MB.</small>
-                </div>
-                <button type="submit" class="btn btn-primary">Create</button>
-            </form>
-        </div>
+        <form action="{{ url('/clients') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="mb-3">
+                <label for="name" class="form-label">Name<span style="color: red">*</span></label>
+                <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required>
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email<span style="color: red">*</span></label>
+                <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required>
+            </div>
+            <div class="mb-3">
+                <label for="phone" class="form-label">Phone<span style="color: red">*</span></label>
+                <input type="tel" class="form-control" id="phone" name="phone" value="{{ old('phone') }}" required
+                       pattern="^\d{3}-\d{3}-\d{4}$">
+                <small style="color: #616467">Format: 123-456-7890</small>
+            </div>
+            <div class="mb-3">
+                <label for="address" class="form-label">Address<span style="color: red">*</span></label>
+                <input type="text" class="form-control" id="address" name="address" value="{{ old('address') }}"
+                       required placeholder="3383 Antigua Ave, Five Islands Village, Antigua & Barbuda">
+            </div>
+            <div class="mb-3">
+                <label for="logo" class="form-label">Company Logo<span style="color: red">*</span></label>
+                <input type="file" class="form-control" id="logo" name="logo" required>
+                <small style="color: #616467">Only image files (e.g. jpg, png) are allowed, and files must be less than 2MB.</small>
+            </div>
+            <button type="submit" class="btn btn-primary">Create</button>
+        </form>
     </div>
 @endsection
