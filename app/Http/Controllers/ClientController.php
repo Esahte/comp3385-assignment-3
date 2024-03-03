ae85b3d549e2e593f7ef790e5bd06b615b715532
Esahtengang Asonganyi <93486453+Esahte@users.noreply.github.com>
2024-03-03 11:30:03 -0400

    Listed new clients along with uploaded logo

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ClientRequest;
+use App\Models\Client;
 
 class ClientController extends Controller
 {
@@ -14,6 +15,17 @@ class ClientController extends Controller
     public function create(ClientRequest $request)
     {
         $request->validated();
+
+        $logo = $request->file('logo');
+        $logo->storeAs('images', $logo->getClientOriginalName(), 'public');
+
+        Client::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'telephone' => $request->phone,
+            'address' => $request->address,
+            'company_logo' => $logo->getClientOriginalName()
+        ]);
 
         return redirect('/dashboard')->with('success', "Client successfully created");
     }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,4 +3,22 @@
 @section('content')
     <h1 class="display-5 fw-bold text-body-emphasis">Dashboard</h1>
     <p class="lead">Welcome to your dashboard. Here you can manage your account, your clients and much more.</p>
+{{--    loog over clients and print out the Clients name, email, tellephone, address and display the company_logo --}}
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+        @foreach($clients as $client)
+            <div class="col">
+                <div class="card">
+                    <img src="{{ asset('storage/' . $client->company_logo) }}" class="card-img-top" alt="{{ $client->name }} Logo">
+                    <div class="card-body">
+                        <h5 class="card-title
+                        ">{{ $client->name }}</h5>
+                        <p class="card-text">{{ $client->email }}</p>
+                        <p class="card-text">{{ $client->phone }}</p>
+                        <p class="card-text">{{ $client->address }}</p>
+                        <a href="{{ route('clients.show', $client->id) }}" class="btn btn-primary">View</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
