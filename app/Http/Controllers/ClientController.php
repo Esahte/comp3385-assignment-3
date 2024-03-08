140e59aae30a1fff0e489bb3f775f82d5758a16f
Esahtengang Asonganyi <93486453+Esahte@users.noreply.github.com>
2024-03-08 15:29:01 -0400

    Added support for mass assigment in my ClientController.php class.

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -19,20 +19,27 @@ class ClientController extends Controller
         $logo = $request->file('logo');
         $logo->storeAs('images', $logo->getClientOriginalName(), 'public');
 
-        $client = new Client;
-        $client->name = $request->name;
-        $client->email = $request->email;
-        $client->telephone = $request->phone;
-        $client->address = $request->address;
-        $client->company_logo = $logo->getClientOriginalName();
-        $client->save();
-//        Client::create([
-//            'name' => $request->name,
-//            'email' => $request->email,
-//            'telephone' => $request->phone,
-//            'address' => $request->address,
+//        $client = new Client;
+//        $client->fill([
+//            'name'         => $request->name,
+//            'email'        => $request->email,
+//            'telephone'    => $request->phone,
+//            'address'      => $request->address,
 //            'company_logo' => $logo->getClientOriginalName()
 //        ]);
+//        $client->name = $request->name;
+//        $client->email = $request->email;
+//        $client->telephone = $request->phone;
+//        $client->address = $request->address;
+//        $client->company_logo = $logo->getClientOriginalName();
+//        $client->save();
+        Client::create([
+            'name'         => $request->name,
+            'email'        => $request->email,
+            'telephone'    => $request->phone,
+            'address'      => $request->address,
+            'company_logo' => $logo->getClientOriginalName()
+        ]);
 
         return redirect('/dashboard')->with('success', "Client successfully created");
     }
