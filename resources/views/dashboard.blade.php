8e87baba356156b9dac4c3056df1ce31a9aac3f6
Esahtengang Asonganyi <93486453+Esahte@users.noreply.github.com>
2024-03-10 17:15:50 -0400

    added design changes to the cards

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,10 +10,11 @@
     <div class="row row-cols-1 row-cols-md-3 g-4">
         @foreach($clients as $client)
             <div class="col">
-                <div class="card text-center h-100">
+                <div class="card text-center h-100"
+                     style="box-shadow: rgba(50, 50, 93, 0.25) 0 6px 12px -2px, rgba(0, 0, 0, 0.3) 0 3px 7px -3px;">
                     <div class="h-70">
-                        <img src="{{ asset('storage/images/' . $client->company_logo) }}" class="card-img-top p-2 h-2"
-                             alt="{{ $client->name }} Logo">
+                        <img src="{{ asset('storage/images/' . $client->company_logo) }}" class="card-img-top p-2"
+                             alt="{{ $client->name }} Logo" style="height: 18rem; object-fit: contain">
                     </div>
                     <div class="card-body" style="background-color: #f6f7f8; padding: 15px;">
                         <h3 class="card-title" style="color: #0d6efd">{{ $client->name }}</h3>
